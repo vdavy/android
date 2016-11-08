5afd63ed7be5d235f69ee5e0dedb712d39676ee7
Vincent DAVY <vdavy.mobile@gmail.com>
2016-11-08 20:06:58 +0100

    Scroll test - to be removed

--- replay/src/main/java/com/stationmillenium/android/replay/activities/ReplayFragment.java
+++ replay/src/main/java/com/stationmillenium/android/replay/activities/ReplayFragment.java
@@ -4,6 +4,8 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,10 @@ import com.stationmillenium.android.replay.dto.TrackDTO;
 import com.stationmillenium.android.replay.utils.view.ReplayAdapter;
 
 import java.util.List;
+
+import static android.support.v7.widget.RecyclerView.SCROLL_STATE_DRAGGING;
+import static android.support.v7.widget.RecyclerView.SCROLL_STATE_IDLE;
+import static android.support.v7.widget.RecyclerView.SCROLL_STATE_SETTLING;
 
 /**
  * Replay fragment
@@ -31,6 +37,30 @@ public class ReplayFragment extends Fragment {
         binding.replayRecyclerview.setAdapter(replayAdapter);
         binding.replaySrl.setColorSchemeResources(R.color.primary, R.color.accent);
         binding.replaySrl.setOnRefreshListener((SwipeRefreshLayout.OnRefreshListener) getActivity());
+        binding.replayRecyclerview.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                Log.v("Scroll DX", String.valueOf(dx));
+                Log.v("Scroll DY", String.valueOf(dy));
+            }
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                switch (newState) {
+                    case SCROLL_STATE_IDLE:
+                        Log.v("Scroll", "SCROLL_STATE_IDLE");
+                        break;
+                    case SCROLL_STATE_DRAGGING:
+                        Log.v("Scroll", "SCROLL_STATE_DRAGGING");
+                        break;
+                    case SCROLL_STATE_SETTLING:
+                        Log.v("Scroll", "SCROLL_STATE_SETTLING");
+                        break;
+                }
+
+
+            }
+        });
         return binding.getRoot();
     }
 
