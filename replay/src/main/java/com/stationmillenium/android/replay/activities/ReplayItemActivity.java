f69a78b44235bf79ed4aa20fd3aca99d69c29391
Vincent <vdavy.mobile@gmail.com>
2016-12-08 17:00:21 +0100

    Init media player

--- replay/src/main/java/com/stationmillenium/android/replay/activities/ReplayItemActivity.java
+++ replay/src/main/java/com/stationmillenium/android/replay/activities/ReplayItemActivity.java
@@ -2,28 +2,37 @@ package com.stationmillenium.android.replay.activities;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.MediaController;
 
 import com.stationmillenium.android.replay.R;
 import com.stationmillenium.android.replay.databinding.ReplayItemActivityBinding;
 import com.stationmillenium.android.replay.dto.TrackDTO;
+
+import java.io.IOException;
 
 /**
  * Activity to play a replay
  * Created by vincent on 28/11/16.
  */
 
-public class ReplayItemActivity extends AppCompatActivity {
+public class ReplayItemActivity extends AppCompatActivity implements android.widget.MediaController.MediaPlayerControl {
 
     private static final String TAG = "ReplayItemActivity";
     public static final String REPLAY_ITEM = "ReplayItem";
 
     private ReplayItemFragment replayItemFragment;
     private ReplayItemActivityBinding replayItemActivityBinding;
+
+    private MediaPlayer mediaPlayer;
+    private MediaController mediaController;
+
+    private TrackDTO replay;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,15 +45,32 @@ public class ReplayItemActivity extends AppCompatActivity {
 
         replayItemFragment = (ReplayItemFragment) getSupportFragmentManager().findFragmentById(R.id.replay_item_fragment);
         extractReplayData();
+
+        mediaPlayer = new MediaPlayer();
+        mediaController = new MediaController(this);
+        mediaController.setMediaPlayer(this);
+        mediaController.setAnchorView(replayItemFragment.getView());
+
+        if (replay != null) {
+            try {
+                mediaPlayer.setDataSource(replay.getStreamURL());
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+                mediaController.setEnabled(true);
+                mediaController.show();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not open file " + replay.getStreamURL() + " for playback.", e);
+            }
+        }
     }
 
     private void extractReplayData() {
         Intent intent = getIntent();
-        TrackDTO replay = (TrackDTO) intent.getSerializableExtra(REPLAY_ITEM);
+        replay = (TrackDTO) intent.getSerializableExtra(REPLAY_ITEM);
         if (replay != null) {
             Log.v(TAG, "Display replay item : " + replay);
             replayItemFragment.setReplay(replay);
-            getSupportActionBar().setTitle(getString( R.string.replay_item_toolbar_title, replay.getTitle()));
+            getSupportActionBar().setTitle(getString(R.string.replay_item_toolbar_title, replay.getTitle()));
         } else {
             Snackbar.make(replayItemActivityBinding.replayItemCoordinatorLayout, R.string.replay_unavailable, Snackbar.LENGTH_SHORT).show();
         }
@@ -52,6 +78,7 @@ public class ReplayItemActivity extends AppCompatActivity {
 
     /**
      * Search tag for replay from replay item
+     *
      * @param tag the tag to search for
      */
     public void searchTag(String tag) {
@@ -60,5 +87,60 @@ public class ReplayItemActivity extends AppCompatActivity {
         searchTagIntent.putExtra(ReplayActivity.REPLAY_TAG, tag);
         startActivity(searchTagIntent);
         finish();
+    }
+
+    @Override
+    public void start() {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void pause() {
+        mediaPlayer.pause();
+    }
+
+    @Override
+    public int getDuration() {
+        return mediaPlayer.getDuration();
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public void seekTo(int pos) {
+        mediaPlayer.seekTo(pos);
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return mediaPlayer.isPlaying();
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0; //mediaPlayer.bu;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return mediaPlayer.getAudioSessionId();
     }
 }
