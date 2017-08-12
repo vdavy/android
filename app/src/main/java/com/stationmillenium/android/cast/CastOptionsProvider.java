b9604e483ae5b102b4d087d29f9ba6d10f39f899
Vincent <vdavy.mobile@gmail.com>
2017-08-12 16:40:54 +0200

    Remove namespace (useless)

--- app/src/main/java/com/stationmillenium/android/cast/CastOptionsProvider.java
+++ app/src/main/java/com/stationmillenium/android/cast/CastOptionsProvider.java
@@ -7,7 +7,6 @@ import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
 import com.stationmillenium.android.R;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,15 +17,10 @@ import java.util.List;
 
 public class CastOptionsProvider implements OptionsProvider {
 
-    public static final String CUSTOM_NAMESPACE = "urn:x-cast:millenium_cast_namespace";
-
     @Override
     public CastOptions getCastOptions(Context appContext) {
-        List<String> supportedNamespaces = new ArrayList<>();
-        supportedNamespaces.add(CUSTOM_NAMESPACE);
         CastOptions castOptions = new CastOptions.Builder()
                 .setReceiverApplicationId(appContext.getString(R.string.app_id))
-                .setSupportedNamespaces(supportedNamespaces)
                 .build();
         return castOptions;
     }
