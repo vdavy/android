9fc0eced124124142f9a7a44af5b7f699d3b4aa9
Vincent DAVY <vdavy.mobile@gmail.com>
2018-09-20 19:22:20 +0200

    Add action and icons on notif

--- app/src/main/java/com/stationmillenium/android/cast/CastOptionsProvider.java
+++ app/src/main/java/com/stationmillenium/android/cast/CastOptionsProvider.java
@@ -6,10 +6,12 @@ import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
 import com.google.android.gms.cast.framework.media.CastMediaOptions;
+import com.google.android.gms.cast.framework.media.MediaIntentReceiver;
 import com.google.android.gms.cast.framework.media.NotificationOptions;
 import com.stationmillenium.android.R;
 import com.stationmillenium.android.activities.CastExpandedControlsActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,8 +23,13 @@ public class CastOptionsProvider implements OptionsProvider {
 
     @Override
     public CastOptions getCastOptions(Context appContext) {
+        List<String> buttonActions = new ArrayList<>();
+        buttonActions.add(MediaIntentReceiver.ACTION_TOGGLE_PLAYBACK);
+        buttonActions.add(MediaIntentReceiver.ACTION_DISCONNECT);
+        int[] compatButtonActionsIndices = new int[]{ 0, 1 };
         NotificationOptions notificationOptions = new NotificationOptions.Builder()
-                .setActions(null, null)
+                .setActions(buttonActions, compatButtonActionsIndices)
+                .setSmallIconDrawableResId(R.drawable.ic_notif_icon)
                 .setTargetActivityClassName(CastExpandedControlsActivity.class.getName())
                 .build();
         CastMediaOptions mediaOptions = new CastMediaOptions.Builder()
