4b0af5d654c0274ca9437d0add19ff0a3209cf87
Vincent DAVY <vdavy.mobile@gmail.com>
2020-05-03 16:08:44 +0200

    Migrate to new API

--- app/src/main/java/com/stationmillenium/android/services/CurrentTitlePlayerService.java
+++ app/src/main/java/com/stationmillenium/android/services/CurrentTitlePlayerService.java
@@ -11,16 +11,18 @@ import androidx.core.app.JobIntentService;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.stationmillenium.android.R;
+import com.stationmillenium.android.dtos.CurrentTrack;
 import com.stationmillenium.android.libutils.AppUtils;
+import com.stationmillenium.android.libutils.DateTime;
 import com.stationmillenium.android.libutils.dtos.CurrentTitleDTO;
-import com.stationmillenium.android.libutils.exceptions.XMLParserException;
 import com.stationmillenium.android.libutils.intents.LocalIntents;
 import com.stationmillenium.android.libutils.intents.LocalIntentsData;
-import com.stationmillenium.android.libutils.network.NetworkUtils;
 import com.stationmillenium.android.libutils.toasts.DisplayToastsUtil;
-import com.stationmillenium.android.libutils.xml.XMLCurrentTitleParser;
 
-import java.io.InputStream;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
 
 import timber.log.Timber;
 
@@ -54,46 +56,49 @@ public class CurrentTitlePlayerService extends JobIntentService {
         if (AppUtils.isMediaPlayerServiceRunning(getApplicationContext())) { //is the media player service running,
             if (AppUtils.isNetworkAvailable(this)) { //is network ok ?
                 Timber.d("Network is available - get XML data...");
-                InputStream is = NetworkUtils.connectToURL(getResources().getString(R.string.player_current_song_url),
-                        null,
-                        getResources().getString(R.string.player_connection_request_method),
-                        getResources().getString(R.string.player_connection_content_type),
-                        Integer.parseInt(getResources().getString(R.string.player_connection_connect_timeout)),
-                        Integer.parseInt(getResources().getString(R.string.player_connection_read_timeout)));
 
-                if (is != null) { //input stream ok
-                    Timber.d("Input stream is OK - process it...");
-                    try {
-                        //get and parse XML data
-                        XMLCurrentTitleParser currentTitleParser = new XMLCurrentTitleParser(is);
-                        CurrentTitleDTO songDataDTO = currentTitleParser.parseXML();
-                        Timber.d("Gathered song data : %s", songDataDTO);
+                try {
+                    RestTemplate restTemplate = new RestTemplate();
+                    restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                    CurrentTrack currentTrack = restTemplate.getForObject(getResources().getString(R.string.player_current_song_url), CurrentTrack.class);
 
-                        //process image if needed
-                        if ((songDataDTO != null)
-                                && (songDataDTO.getCurrentSong() != null)
-                                && (songDataDTO.getCurrentSong().getMetadata() != null)) {
-                            songDataDTO.getCurrentSong().setImageURL(getResources().getString(R.string.player_image_url_root) + songDataDTO.getCurrentSong().getMetadata().getPath());
+                    CurrentTitleDTO currentTitleDTO = new CurrentTitleDTO();
+
+                    if (currentTrack != null && currentTrack.isTrack()) {
+                        currentTitleDTO.getCurrentSong().setArtist(currentTrack.getArtist());
+                        currentTitleDTO.getCurrentSong().setTitle(currentTrack.getTitle());
+                        currentTitleDTO.getCurrentSong().setPlayedDate(new Date(DateTime.parseRfc3339(currentTrack.getTime()).getValue()));
+                        if (currentTrack.isImage()) {
+                            currentTitleDTO.getCurrentSong().setImageURL(getResources().getString(R.string.player_image_url_root) + currentTrack.getTime());
                         }
 
-                        //send intent
-                        Intent intentToSend = new Intent(LocalIntents.CURRENT_TITLE_UPDATED.toString());
-                        intentToSend.putExtra(LocalIntentsData.CURRENT_TITLE.toString(), songDataDTO);
-                        Timber.d("Send intent to update current title : %s", intentToSend);
+                        CurrentTrack[] last5Tracks = restTemplate.getForObject(getResources().getString(R.string.player_last_5_tracks_url), CurrentTrack[].class);
+                        for (CurrentTrack historyTrack : last5Tracks) {
+                            if (historyTrack.isTrack()) {
+                                CurrentTitleDTO.Song song = new CurrentTitleDTO.Song();
+                                song.setArtist(historyTrack.getArtist());
+                                song.setTitle(historyTrack.getTitle());
+                                song.setPlayedDate(new Date(DateTime.parseRfc3339(historyTrack.getTime()).getValue()));
+                                song.setImageURL(getResources().getString(R.string.player_image_url_root) + historyTrack.getTime());
 
-                        LocalBroadcastManager.getInstance(this).sendBroadcast(intentToSend);
-
-                    } catch (XMLParserException e) {
-                        Timber.w(e, "Error while parsing XML data");
+                                currentTitleDTO.getHistory().add(song);
+                            }
+                        }
                     }
 
-                } else { //error while getting input stream
-                    Timber.d("No input stream - stopping service...");
+                    //send intent
+                    Intent intentToSend = new Intent(LocalIntents.CURRENT_TITLE_UPDATED.toString());
+                    intentToSend.putExtra(LocalIntentsData.CURRENT_TITLE.toString(), currentTitleDTO);
+                    Timber.d("Send intent to update current title : %s", intentToSend);
+
+                    LocalBroadcastManager.getInstance(this).sendBroadcast(intentToSend);
+
+                } catch (Exception e) {
+                    Timber.w(e, "Error getting current track");
                 }
 
             } else {
                 Timber.d("Network is unavailable - stopping service...");
-
                 displayToast.displayToast(R.string.player_network_unavailable);
             }
 
@@ -101,5 +106,4 @@ public class CurrentTitlePlayerService extends JobIntentService {
             Timber.d("Media player is not running - stopping service...");
         }
     }
-
 }
